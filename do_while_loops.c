2bc60393c87fdb8b0a11219b1975106d05a5847b
Alex Lu <142097738+Dequ@users.noreply.github.com>
2024-02-09 10:26:42 +0800

    Create do_while_loops.c

---
+++ do_while_loops.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{   
+    int num, inner_counter, outer_counter;
+    scanf("%d", &num);
+    outer_counter = 1;
+    if (num > 20)
+        num = 20;
+	do {
+	    inner_counter = 1;
+	    outer_counter++;
+	    do {
+	        printf("*#");
+	        inner_counter++;
+	    } while (inner_counter < outer_counter);
+	    printf("\n");
+	} while (outer_counter - 1 < num); 
+	return 0;
+}
